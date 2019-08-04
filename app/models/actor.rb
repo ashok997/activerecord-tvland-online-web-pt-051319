4c54ac307db9036f9e6f47900e052f864f48e307
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-04 23:54:28 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,9 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    self.characters
+    self.characters.collect do |character|
+      charcter.name
+      character.show_id.name
+    end
   end
 end
