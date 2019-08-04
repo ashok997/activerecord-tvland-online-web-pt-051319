9d015633da41ac39b297f7b36e368c01d9a81f46
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-04 23:29:34 +0000

    Automatically backed up by Learn

--- db/migrate/005_add_catchphrase_to_character.rb
+++ db/migrate/005_add_catchphrase_to_character.rb
@@ -1,7 +1,7 @@
 class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   #
   def change
-    add_column :characters, :network_id, :integer
+    add_column :characters, :catchphrase, :string
   end
   
 end
