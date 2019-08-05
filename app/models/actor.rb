78a5849f5e7d1e780cc6b3bd9b6778993965210b
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-05 00:11:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,8 +8,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     self.characters.collect do |character|
-      #character.name
-      character.show.name
+      role = "#{character.name}" +"-"+ "#{character.show.name}"
     end    
   end
 end
